003fe9c3bd86dc0fb21a41a86300eaee9e65bb78
rodrigo <rodrigocamargo854@gmail.com>
2022-07-30 08:30:01 -0300

    (home) initial style ok

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,15 @@
+import Link from 'next/link';
 import { Logo } from '../Logo';
 import styles from './header.module.scss';
 
 export default function Header() {
   return (
-    <header>
-      <div className={styles.header}>
-        <Logo />
-      </div>
+    <header className={styles.postHeader}>
+      <Link href="/">
+        <a href="">
+          <img src="/images/logo.svg" alt="logo" />
+        </a>
+      </Link>
     </header>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from 'next';
-import { Head } from 'next/document';
+import Head from 'next/head';
+import Link from 'next/link';
 import Header from '../components/Header';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -29,13 +31,34 @@ interface HomeProps {
 export default function Home() {
   return (
     <>
-      <Header/>
+      <main className={commonStyles.container}>
+        <Header />
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Como Utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 mar 2201
+                </li>
+                <li>
+                  <FiUser />
+                  Rodrigo Camargo
+                </li>
+              </ul>
+            </a>
+          </Link>
+
+        </div>
+      </main>
     </>
   );
 }
 
 // export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
-//   // TODO
+// const prismic = getPrismicClient({});
+// const postsResponse = await prismic.getByType(TODO);
+// TODO
 // };
